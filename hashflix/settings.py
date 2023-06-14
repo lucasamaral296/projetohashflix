1da4c4232f8fde6d8f3d8fbdb5f0ae2796ce1405
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-06-13 21:52:03 -0300

    Update settings.py

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -100,12 +100,12 @@ DATABASES = {
 }
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, )
-    }
+# if DATABASE_URL:
+#     DATABASES = {
+#         'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, )
+#     }
 
 
 

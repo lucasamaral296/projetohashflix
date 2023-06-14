075e3b53593dc18ec4066678281209c38eda9a5c
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-13 21:01:11 -0300

    ajustes banco sql

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -87,19 +87,25 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'u7IJn7vUY1TEd6eMo2yr',
+        'HOST': 'containers-us-west-8.railway.app',
+        'PORT': '7755',
     }
+}
+
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+#
+# if DATABASE_URL:
+#     DATABASES = {
+#         'defaut': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, )
+#     }
 
 
 
